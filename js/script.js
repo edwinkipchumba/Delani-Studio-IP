68195b0aaff99448a01909759d05e5937afec23a
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-07-24 12:59:23 +0300

    design hidden added

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+     $(".mouse").mouseover(function(){
+         $("#design-hidden").toggle();
+         $("#design-show").toggle();
+     })
+
+
+    // $("#form").submit(function(event){
+    //     event.preventDefault();
+    //     console.log("form has been submitted!");
+
+    //     var nameInput =$("inputed#name").val();
+    //     var emailInput =$("inputed#mail").val();
+    //     var textareaInput =$("inputed#message").val();
+    //     $("inputed#name").text
+    // })
+})
